e2c4f3f3237ed6d3f5bc60c5bde16ed0f0190a9f
Greg Rafferty <greg.rafferty+git@gmail.com>
2020-05-16 20:11:07 -0700

    set debug to false

--- app.py
+++ app.py
@@ -788,4 +788,4 @@ app.layout = html.Div(style={'backgroundColor': dash_colors['background']}, chil
         ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
